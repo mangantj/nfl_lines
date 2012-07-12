59d4eeb7acbf1e10e9651e3f012c62229540837b
mangantj <mangantj@gmail.com>
2012-07-12 14:39:04 -0500

    Added hash to convert id to team name. Changed week index to have links for each week.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,39 @@ class Game < ActiveRecord::Base
   has_many :picks, :dependent => :destroy
   accepts_nested_attributes_for :picks
   attr_accessible :week_id, :away_team_id, :home_team_id, :date, :picks_attributes
+
+  TEAM_NAME = {
+    '1' => '49ers',
+    '2' => 'Bears',
+    '3' => 'Bengals',
+    '4' => 'Bills', 
+    '5' => 'Broncos',
+    '6' => 'Browns', 
+    '7' => 'Bucaneers',
+    '8' => 'Cardinals',
+    '9' => 'Chargers',
+    '10' => 'Cheifs',
+    '11' => 'Colts',
+    '12' => 'Cowboys',
+    '13' => 'Dolphins',
+    '14' => 'Eagles',
+    '15' => 'Falcons', 
+    '16' => 'Giants', 
+    '17' => 'Jaguars',
+    '18' => 'Jets',
+    '19' => 'Lions',
+    '20' => 'Packers',
+    '21' => 'Panthers',
+    '22' => 'Patriots',
+    '23' => 'Raiders',
+    '24' => 'Rams',
+    '25' => 'Ravens',
+    '26' => 'Redskins',
+    '27' => 'Saints',
+    '28' => 'Seahawks',
+    '29' => 'Steelers', 
+    '30' => 'Texans',
+    '31' => 'Tians',
+    '32' => 'Vikings'
+  }
 end
