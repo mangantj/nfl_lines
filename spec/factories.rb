40556d122fb870e8c97256e704e3012c363208fe
mangantj <mangantj@gmail.com>
2012-07-14 20:50:36 -0500

    Commiting errors occurring with factory girl.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,29 @@
 require 'factory_girl'
 
 FactoryGirl.define do
-  factory :pick do |pick|
-    pick.user_id 1
-    pick.game_id 1
-    pick.week_id 1
-    pick.spread 4
+  factory :week do
+    year 2012
+    start_date {8.days.ago}
+    end_date {0.days.ago}
+    before(:build)  do |week|
+      week.games = FactoryGirl.create_list(:game, week: week)
+    end
+  end
+
+  factory :game, :parent => :week do
+    away_team_id 1
+    home_team_id 2
+    date {4.days.ago}
+    week
+    before(:build) do |game|
+      game.picks = FactoryGirl.create_list(:pick, game: game)
+    end
+  end
+
+  factory :pick do
+    user_id 1
+    spread 4
+    game
+    week
   end
 end
--- spec/models/pick_spec.rb
+++ spec/models/pick_spec.rb
@@ -2,24 +2,25 @@ require 'spec_helper'
 
 describe Pick do 
   before(:each) do
-    @pick = FactoryGirl.create(:pick)
+    @week = FactoryGirl.create(:week)
   end
 
   it "should not allow spread value that is not integer or half of integer" do
-    @pick.spread = 4.4
-    @pick.should_not be_valid
-    @pick.spread = -4.4
-    @pick.should_not be_valid
+    p @week.games
+    @week.games.pick.spread = 4.4
+    @week.games.pick.should_not be_valid
+    @week.games.pick.spread = -4.4
+    @week.games.pick.should_not be_valid
   end
 
   it "should allow for negative spread" do
-    @pick.spread = -4.5
-    @pick.should be_valid
+    @week.games.pick.spread = -4.5
+    @week.games.pick.should be_valid
   end
 
   it "should not allow a nil/blank spread" do
-    @pick.spread = nil
-    @pick.should_not be_valid
+    @week.games.pick.spread = nil
+    @week.games.pick.should_not be_valid
   end
   
 end
